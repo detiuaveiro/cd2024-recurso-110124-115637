2923f6bb0dc6f09fb975eec37005504817ed753c
edivaldolluisb <edivaldoluisb16@gmail.com>
2024-06-26 22:26:57 +0100

    update worker for remote redis connection

--- src/worker.py
+++ src/worker.py
@@ -1,7 +1,17 @@
 import argparse
 from tasks import app
+import logging
+
+logging.basicConfig(level=logging.INFO, filename="worker.log")
+logger = logging.getLogger(__name__)
 
 def main(broker_address):
+    host = broker_address.split(":")[0]
+    if "@" not in host:
+        print(f"Running worker with host {host}")
+        logger.info(f"Running worker with host {host}")
+        app.conf.update(backend=f"redis://{host}:6379/0")
+
     app.conf.update(broker=f"pyamqp://{broker_address}")
 
     app.start(argv=["worker"])
