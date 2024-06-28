a276ecc6a784691a3fec898f81449e68279d6254
edivaldolluisb <edivaldoluisb16@gmail.com>
2024-06-29 00:02:02 +0100

    FIX: update worker

--- src/worker.py
+++ src/worker.py
@@ -6,7 +6,7 @@ import uuid
 logging.basicConfig(level=logging.INFO, filename="worker.log")
 logger = logging.getLogger(__name__)
 
-def main(broker_address, worker_name):
+def main(broker_address, worker_name, concurrency=None, autoscale=None):
     host = broker_address.split(":")[0]
     if "@" not in host:
         print(f"Running worker with host {host}")
@@ -15,7 +15,15 @@ def main(broker_address, worker_name):
 
     app.conf.update(broker=f"pyamqp://{broker_address}")
 
-    app.start(argv=["worker", "--loglevel=info", "-n", worker_name])
+    argv=["worker", "--loglevel=info", "-n", worker_name]
+    if concurrency:
+        argv.extend(["-c", str(concurrency)])
+        
+    if autoscale:
+        argv.extend(["--autoscale", autoscale ])
+
+
+    app.start(argv)
 
 if __name__ == "__main__":
 
@@ -23,11 +31,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Sudoku Solver Worker")
     parser.add_argument(
         "-b", "--broker", type=str, help="Broker address", default="guest@localhost")
-    id = str(uuid.uuid4())
-    worker_name = f"worker{id}"
+
     parser.add_argument(
-        "-n", "--name", type=str, help="Worker name", default=worker_name)
+        "-n", "--name", type=str, help="Worker name", default=f"worker{uuid.uuid4()}")
+    
+    parser.add_argument(
+        "-c", "--concurrency", type=int, help="Worker concurrency")
+    
+    # add auto scaling
+    parser.add_argument(
+        "-a", "--autoscale", type=str, help="Worker autoscale: max,min")
+    
     args = parser.parse_args()
 
-
-    main(args.broker, args.name)
+    # call main with concurrency and autoscale if provided, dont add them to the args if they are None
+    main(args.broker, args.name, concurrency=args.concurrency, autoscale=args.autoscale)
