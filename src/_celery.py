3e0d1063ba03fa9ac5591c833bf53306babf2af9
edivaldolluisb <edivaldoluisb16@gmail.com>
2024-06-28 14:07:51 +0100

    UPDATE worker

--- src/_celery.py
+++ src/_celery.py
@@ -11,6 +11,12 @@ app.conf.update(
     accept_content=['json'],
     timezone='Europe/Lisbon',
     enable_utc=True,
+    broker_connection_retry_on_startup=True, # retry connection on startup
+    worker_prefetch_multiplier=1, # 1 task per worker
+    worker_concurrency=1, # 1 worker
+    # worker_max_tasks_per_child=1, # worker will be restarted after 1 task
+    task_reject_on_worker_lost=True, # reject task if worker is lost, so it can be retried by another worker
+
 )
 
 
